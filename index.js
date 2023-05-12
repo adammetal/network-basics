e5bcaf1a8008f0f2f0853e050b64abc36b579c69
adam.metal <adam.metal@codecool.com>
2023-05-12 12:18:39 +0200

    Add more http method

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = document.querySelector("#app");
 
 // General api getter function
 // Util layer
-// Api layer
+// ------- API LAYER ---------------
 function apiGet(url) {
   return fetch(url)
     .then(function (res) {
@@ -13,6 +13,52 @@ function apiGet(url) {
     });
 }
 
+function apiPost(url, data) {
+  return fetch(url, {
+    method: 'POST',
+    header: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data),
+  })
+  .then(function(res){
+    return res.json();
+  })
+  .then(function(data) {
+    return data;
+  });
+}
+
+function apiDelete(url) {
+  return fetch(url, {
+    method: 'DELETE'
+  })
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      return data;
+    });
+}
+
+function apiPatch(url, data) {
+  return fetch(url, {
+    method: 'PATCH',
+    header: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data),
+  })
+  .then(function(res){
+    return res.json();
+  })
+  .then(function(data) {
+    return data;
+  });
+}
+
+
+
 // ------ SERVICE LAYER ------ (MODEL)
 
 // Specialized
@@ -21,7 +67,20 @@ function getTodos() {
   return apiGet(TODOS_URL);
 }
 
-// Specialized
+function setTodoComplete(id) {
+  const url = "https://jsonplaceholder.typicode.com/todos/" + id;
+  return apiPatch(url, {
+    completed: true
+  });
+}
+
+function setTodoInComplete(id) {
+  const url = "https://jsonplaceholder.typicode.com/todos/" + id;
+  return apiPatch(url, {
+    completed: false
+  });
+}
+
 function getKittens() {
   const CAT_URL = 'https://api.thecatapi.com/v1/images/search'
   return apiGet(CAT_URL);
@@ -44,12 +103,26 @@ function createTodoElement(todo) {
   check.type = 'checkbox';
   check.checked = todo.completed;
 
+  check.addEventListener('change', function(event){
+    const id = todo.id;
+    const checked = event.target.checked;
+    handleTodoCheckboxClick(id, checked);
+  });
+
   todoEl.append(span, check);
   return todoEl;
 }
 
 
 // ------ CONTROLLER ---------------
+function handleTodoCheckboxClick(id, checked) {
+  if (checked === true) {
+    setTodoComplete(id);
+  } else {
+    setTodoInComplete(id);
+  }
+}
+
 function main() {
   const todosPromise = getTodos();
   const catsPromise = getKittens();
